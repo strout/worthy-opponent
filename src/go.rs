8f3d16d25dc6d33e2921049f1527654302cbb456
Steve Trout <steve.trout@gmail.com>
2015-11-01 21:41:28 -0600

    Keeping things consistent.

--- src/go.rs
+++ src/go.rs
@@ -76,7 +76,7 @@ fn capture_inner(c: Space, p: Pos, b: &Board, captured: &mut BitSet) -> bool {
             Empty => return false,
             c2 => if c == c2 {
                 captured.insert(p);
-                for p in neighbors(p).iter().filter_map(Option::clone) {
+                for &p in neighbors(p).iter().flat_map(|x| x) {
                     if !capture_inner(c, p, b, captured) { return false }
                 }
             }
