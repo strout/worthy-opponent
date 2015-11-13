6fb36c5feb9989bc60b2940afe916928260226d9
Steve Trout <steve.trout@gmail.com>
2015-11-13 16:04:27 -0600

    Get rid of the bitrotting server.

--- worthy-server/serve.sh
+++
@@ -1,8 +0,0 @@
-mkfifo /tmp/player1 /tmp/player2 > /dev/null 2>&1
-tee p1_in < /tmp/player1 | $1 | tee p1_out | nc 127.0.0.1 64335 > /tmp/player1 &
-pid1=$!
-tee p2_in < /tmp/player2 | $2 | tee p2_out | nc 127.0.0.1 64335 > /tmp/player2 &
-pid2=$!
-while kill -0 $pid1 $pid2 > /dev/null 2>&1; do sleep 1; done
-kill $pid1 $pid2 > /dev/null 2>&1
-rm /tmp/player1 /tmp/player2 > /dev/null 2>&1
